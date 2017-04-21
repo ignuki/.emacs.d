a4621de3de45a76817307835c9430730f7d67ce1
Iñaki Domínguez <inakidomoc@gmail.com>
2017-04-22 00:08:03 +0200

    autoinstall packages

--- my-packages.el
+++ my-packages.el
@@ -8,6 +8,25 @@
   (normal-top-level-add-subdirs-to-load-path))
 (package-initialize)
 
-(unless (package-installed-p 'use-package)
+(defvar prelude-packages
+  '(projectile use-package ivy cl evil magit evil-magit fill-column-indicator
+               all-the-icons haskell-mode ghc web-mode less-css-mode base16-emacs)
+  "A list of packages to ensure are installed at launch.")
+
+(defun prelude-packages-installed-p ()
+  (if (member
+        nil '(mapcar
+              'package-installed-p
+              prelude-packages))
+    nil
+    t))
+
+(unless (prelude-packages-installed-p)
+ check for new packages (package versions)
+  (message "%s" "Emacs Prelude is now refreshing its package database...")
   (package-refresh-contents)
-  (package-install 'use-package))
+  (message "%s" " done.")
+ install the missing packages
+  (dolist (p prelude-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
