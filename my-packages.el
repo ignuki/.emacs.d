37e0faf6ad5f2705a036b6440ad8a1b499d42d88
inaki <inaki@prometheus>
2017-06-06 12:30:16 +0200

    Finally fixed autoinstall

--- my-packages.el
+++ my-packages.el
@@ -1,7 +1,7 @@
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/") t
 (add-to-list 'load-path "~/.emacs.d/lisp/" t)
 
@@ -15,22 +15,6 @@
 		  pkg-info projectile swiper undo-tree use-package use-package-chords
 		  web-mode with-editor)
   "A list of packages to ensure are installed at launch.")
-
-
-
-;; (defun prelude-packages-installed-p ()
-;;   (let lst (mapcar 'package-installed-p prelude-packages)
-;;        (if (member nil 'lst)
-;; 	   nil
-;; 	 t)))
-
-;;(unless (prelude-packages-installed-p)
-;;  (message "%s" "Emacs Prelude is now refreshing its package database...")
-;;  (package-refresh-contents)
-;;  (message "%s" " done.")
-;;  (dolist (p prelude-packages)
-;;    (when (not (package-installed-p p))
-;;      (package-install p))))
 
 (dolist (pkg prelude-packages)
  (unless (package-installed-p pkg)
