fb6239e4a036df71ee865160d434b3ef53dd03ec
Iñaki <inaki694@gmail.com>
2017-06-09 12:48:20 +0200

    remove ghc and cabal deps

--- my-noexternals.el
+++ my-noexternals.el
@@ -18,10 +18,6 @@
       '(buffer-file-name "%f" (dired-directory dired-directory "%b"))
       ;; (setq debug-on-error t)
       auth-source-save-behavior nil
-      haskell-process-auto-import-loaded-modules t
-      haskell-process-log t
-      haskell-process-suggest-remove-import-lines t
-      haskell-process-type (quote cabal-repl)
       send-mail-function (quote smtpmail-send-it)
       enable-local-variables nil
       inhibit-startup-screen t
@@ -118,7 +114,6 @@
 (add-hook 'emacs-lisp-mode-hook 'remove-elc-on-save)
 (add-hook 'c-mode-hook 'my-c-mode-hooks)
 (add-hook 'web-mode-hook  'my-web-mode-hook)
-(add-hook 'haskell-mode-hook (lambda () (ghc-init)))
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (add-hook 'eshell-mode-hook (lambda ()
@@ -136,28 +131,3 @@
   (process-send-string
    (get-buffer-process (current-buffer))
    (if string string (current-kill 0))))
-
-(eval-after-load 'haskell-mode
-  '(progn
-     (define-key haskell-mode-map (kbd "C-c C-l")
-       'haskell-process-load-or-reload)
-     (define-key haskell-mode-map (kbd "C-c C-z")
-       'haskell-interactive-switch)
-     (define-key haskell-mode-map (kbd "C-c C-n C-t")
-       'haskell-process-do-type)
-     (define-key haskell-mode-map (kbd "C-c C-n C-i")
-       'haskell-process-do-info)
-     (define-key haskell-mode-map (kbd "C-c C-n C-c")
-       'haskell-process-cabal-build)
-     (define-key haskell-mode-map (kbd "C-c C-n c")
-       'haskell-process-cabal)))
-(eval-after-load 'haskell-cabal
-  '(progn
-     (define-key haskell-cabal-mode-map (kbd "C-c C-z")
-       'haskell-interactive-switch)
-     (define-key haskell-cabal-mode-map (kbd "C-c C-k")
-       'haskell-interactive-mode-clear)
-     (define-key haskell-cabal-mode-map (kbd "C-c C-c")
-       'haskell-process-cabal-build)
-     (define-key haskell-cabal-mode-map (kbd "C-c c")
-       'haskell-process-cabal)))
