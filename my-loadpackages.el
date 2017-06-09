a39f93a174b9707abdc4e78611a5f6e57c881d39
Iñaki <inaki694@gmail.com>
2017-06-09 13:04:25 +0200

    fix package config

--- my-loadpackages.el
+++ my-loadpackages.el
@@ -1,16 +1,15 @@
 (require 'use-package)
 
 (use-package ivy
-  :init
+  :config
   (setq ivy-use-virtual-buffers t
         ivy-count-format ""
         ivy-display-style nil)
-  :config
   (ivy-mode 1))
 (use-package projectile)
 (use-package cl)
 (use-package evil
-  :init
+  :config
   (setq evil-normal-state-tag    "N"
         evil-insert-state-tag    "I"
         evil-visual-state-tag    "V"
@@ -18,16 +17,15 @@
         evil-operator-state-tag  "O"
         evil-motion-state-tag    "M"
         evil-replace-state-tag   "R"
-        evil-want-fine-undo      'fine )
-  :config
+        evil-want-fine-undo      t )
   ;;  (add-to-list 'evil-insert-state-modes 'lisp-interaction-mode)
   (evil-mode 1))
 (use-package magit)
 (use-package evil-magit
-  :init
+  :config
   (setq evil-magit-state 'normal))
 (use-package fill-column-indicator
-  :init
+  :config
   (setq fci-rule-column 80))
 (use-package all-the-icons)
 (use-package haskell-mode)
